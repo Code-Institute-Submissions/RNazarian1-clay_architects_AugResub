6d43ce2c3465a2f8a0b350413f4ca8a8de5d409f
RNazarian1 <nazarianreza@gmail.com>
2021-04-11 22:11:31 +0000

    markers are set and labels are placed and map zoom is set

--- assets/js/whoarewe.js
+++ assets/js/whoarewe.js
@@ -18,7 +18,27 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("gmap"), {
-    center: { lat: 53.2194, lng: 6.5665 },
-    zoom: 8,
+    center: { lat: 53.99775, lng: 2.4966},
+    zoom: 5,
   });
+
+
+var lables="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var locations=[
+    { lat: 53.2194, lng: 6.5665 },
+    { lat: 54.7761, lng:-1.5733 },
+];
+var markers=locations.map(function(location,i){
+    return new google.maps.Marker({
+        position:location,
+        label: lables[i%lables.length]
+
+    });
+
+});
+  new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+
 }
