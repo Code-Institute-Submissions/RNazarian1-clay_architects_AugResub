631974326f27c440dab88b4260d13049faf5c340
RNazarian1 <nazarianreza@gmail.com>
2021-04-18 14:56:37 +0000

    home intro background is removed

--- assets/js/whoarewe.js
+++ assets/js/whoarewe.js
@@ -1,18 +1,5 @@
 console.log("Hello");
 
-// function newFunction() {
-//     return "black";
-// }
-
- 
-
-// var list = document.getElementsByTagName("ul")[0];
-// console.log(list);
-// for (n = 0; 6; n++) {
-//     list.getElementsByTagName("li")[n].getElementsByTagName("i")[0].style.color = newFunction();
-//     list.getElementsByTagName("li")[n].getElementsByTagName("i")[0].style.borderColor = newFunction();
-// }
-//  console.log(list);
 
 let map;
 
